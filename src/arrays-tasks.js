0d6014624eccb09c3d71cf581cc933b0c64e7ed7
kristina-trakhimchik <dilfinium_kristi@mail.ru>
2024-01-15 00:23:41 +0300

    feat: solve some extra tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -90,8 +90,16 @@ function findAllOccurrences(arr, item) {
  *    removeFalsyValues([ 1, 2, 3, 4, 5, 'false' ]) => [ 1, 2, 3, 4, 5, 'false' ]
  *    removeFalsyValues([ false, 0, NaN, '', undefined ]) => [ ]
  */
-function removeFalsyValues(/* arr */) {
-  throw new Error('Not implemented');
+function removeFalsyValues(arr) {
+  return arr.filter(
+    (x) =>
+      x !== false &&
+      x !== null &&
+      x !== '' &&
+      x !== undefined &&
+      !Number.isNaN(x) &&
+      x !== 0
+  );
 }
 
 /**
@@ -104,8 +112,8 @@ function removeFalsyValues(/* arr */) {
  *    getStringsLength([ '', 'a', 'bc', 'def', 'ghij' ]) => [ 0, 1, 2, 3, 4 ]
  *    getStringsLength([ 'angular', 'react', 'ember' ]) => [ 7, 5, 5 ]
  */
-function getStringsLength(/* arr */) {
-  throw new Error('Not implemented');
+function getStringsLength(arr) {
+  return arr.map((x) => x.length);
 }
 
 /**
@@ -122,8 +130,16 @@ function getStringsLength(/* arr */) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  const sum = 0;
+  if (arr.length === 0) {
+    return 0;
+  }
+  const res = arr.reduce((x, y) => x + y, sum) / arr.length;
+  if (Number.isInteger(res)) {
+    return res;
+  }
+  return Number(res.toFixed(2));
 }
 
 /**
@@ -136,8 +152,9 @@ function getAverage(/* arr */) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  const len = arr[0].length;
+  return arr.every((x) => x.length === len);
 }
 
 /**
